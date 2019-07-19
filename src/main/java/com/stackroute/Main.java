454334cd20d4f6303d18bf0b6efe8188500d5981
sankeerthana <15j41a05n0@gmail.com>
2019-07-19 09:57:56 +0530

    submission

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,16 +1,21 @@
 package com.stackroute;
 
 import com.stackroute.demo.BeanLifeCycleDemo;
+import com.stackroute.demo.BeanPostProcessorDemo;
+import com.stackroute.domain.Movie;
 
-import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.core.io.ClassPathResource;
 public class Main {
     public  static  void  main(String[] args)
     {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
+        ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+
         BeanLifeCycleDemo beanLifeCycleDemo=(BeanLifeCycleDemo) applicationContext.getBean("lifeCycle");
+        BeanPostProcessorDemo bean=(BeanPostProcessorDemo) applicationContext.getBean("bean");
+        applicationContext.close();
 
 
 
